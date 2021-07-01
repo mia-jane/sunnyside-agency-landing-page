fcd1c0fe153af733d12304102275854c710c152d
Mia Stroud <miast36@gmail.com>
2021-07-01 12:20:03 -0400

    added index js file that toggles menu drpdwn

---
+++ index.js
@@ -0,0 +1,6 @@
+// document.querySelector(".dropdown-content").className += " "
+console.log(document.querySelector(".dropdown-content"))
+
+const toggleDropdownMenu = () => {
+  document.querySelector(".dropdown-content").classList.toggle("show")
+}
